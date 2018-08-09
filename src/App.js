a958c697f1cbf1364927a378cbd6d1e887d9e542
Christian Piegert <cpiegert@UNINORTEACAD.UNINORTE.EDU.CO>
2018-08-09 16:19:42 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+    <main>
+	<title>Hello</title>
+	<p> Hola mundo, soy yo</p>
+	</main>
+    );
+  }
+}
+
+export default App;
